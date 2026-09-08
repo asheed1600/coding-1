6cf6e41692b403a17c95d5eb3b07191067718d32
asheed1600 <asheed584@boyslatin.net>
2026-09-08 14:59:26 +0000

    updating github

---
+++ unit 1/variableactivity.py
@@ -0,0 +1,14 @@
+applecost = 1.00
+numberofapple = 10 
+
+ja3ssneakersCOUNT = 9
+ja3ssneakersPRICE = 150.00
+ja3ssneakersSKU = "9102238"
+
+
+# wehm we want to create a variable  we make up a word 
+# use the equal sign and then assign it a date type 
+#
+
+
+user "aiden sheed"
