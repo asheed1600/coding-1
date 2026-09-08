7fa02396ce1ccdd076ac28630b76a1f03f16d972
asheed1600 <asheed584@boyslatin.net>
2026-09-08 14:59:02 +0000

    updating github

---
+++ unit 1/datetypes.py
@@ -0,0 +1,34 @@
+# STRING - characters that are wrapped within quotes 
+# characters can be letters numbers and symbols 
+# you can use both single and double  quotation 
+# marks for string
+
+"aiden sheed "
+
+'123 main street'
+'this cost $100 dollars.'
+
+# the hashtag ia a marker that makes anything written 
+# behind it invisiable to the computer 
+
+# INTEGERS- datetypes that are just whole numbers 
+# or numbers without a decimal point. we dont have 
+# to add any special symbols we can write the number 
+# as it is 
+
+10
+20
+20000000
+
+# FLOATS - A  number with a decimal 
+20.99
+3.339
+0.23
+
+# BOOLEAN -  a data type that can only be either 
+# true or false we do not use quotation marks for 
+# boolean valuse the computer understand these words 
+# as something unique 
+
+Fales 
+ture 
