3d55b882518bf1db1a1e1e3192c50bbb785bff81
asheed1600 <asheed584@boyslatin.net>
2026-09-10 15:12:50 +0000

    class notes

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -4,7 +4,8 @@ think key / value pairings
 
 we use a single equal sign to reprsent the assinment 
 name = "aiden sheed"
-
+grade = 10th 
+school true 
 
 
 
@@ -18,3 +19,47 @@ name = "aiden sheed"
 
 # print is a functiom that lets us show code 
 # in the terminal 
+
+
+
+
+#comparison opertors - set of symbols used 
+#to assess if data is the same or diffrent and 
+# howthey differ 
+
+print((10 > 1) # greater then operator 
+print (2000 < 100) # less then operator 
+
+
+# 2 equal signs compare if something is the SAME  
+print ("book" == "Book") # same as (false)
+print ("2" == 2) #same as (false)
+print (2.0  ==  2) same as (true)
+
+#not equal is written with !=
+# this is to check and fillter for values that are not 
+# the same 
+print(200  != 100) # TRUE - these are not the same 
+print (300  != 300) # false - these are the same 
+
+
+
+
+
+# logical operaters - compares 2 conditions to check if 
+#they are true or false 
+# conditions = other operaters
+# instead of symbols we reprsent these with words 
+# and or not 
+
+#AND - checks if 2 conditions are true if yes the final 
+# result is true 
+print (3 > 1 and 100 == 50) # this would come to be true 
+# OR - checks if only 1 condition is true if yes 
+# the final result will true 
+print (3 > 1 or 100 == 50) 
+
+# NOT  - the "opposite day" operator it will reverse the 
+# reslut of the logical operators 
+print (not ( 3 > 1 and 100 > 50) 
+# this would come out to be false 
